cbbe5eb7c7ce89323dd53b73079c67bd988f30d3
Ranil-Valeev <valeevranil08@gmail.com>
2026-02-15 13:08:40 +0300

    start

---
+++ Сourse_management/Сourse_management/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Сourse_management.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : Page
+    {
+        private Frame _frame;
+        public RegisterPage(Frame frame )
+        {
+            InitializeComponent();
+            _frame = frame;
+        }
+    }
+}
---
+++ Сourse_management/Сourse_management/Pages/StartPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Сourse_management.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        private Frame _frame;
+        public StartPage(Frame frame)
+        {
+            InitializeComponent();
+            _frame = frame;
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            _frame.Navigate(new Pages.LoginPage(_frame));
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            _frame.Navigate(new Pages.RegisterPage(_frame));
+        }
+
+        private void Telegram_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://t.me/your_channel";
+
+            Process.Start(new ProcessStartInfo(url)
+            {
+                UseShellExecute = true
+            });
+        }
+    }
+}
